01ee9287c189843d8f03e3f38cf19aa2da7933fa
JuanCarlos <juan.carlos.marques.pereira@gmail.com>
2026-06-30 21:34:00 -0300

    Cria uma entidade no banco de dados apartir do NinjaModel

--- src/main/java/com/juan/cadastrodeninjas/controller/NinjaController.java
+++ src/main/java/com/juan/cadastrodeninjas/controller/NinjaController.java
@@ -1,4 +1,4 @@
-package com.juan.CadastroDeNinjas.controller;
+package com.juan.cadastrodeninjas.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/test/java/com/juan/cadastrodeninjas/CadastroDeNinjasApplicationTests.java
+++ src/test/java/com/juan/cadastrodeninjas/CadastroDeNinjasApplicationTests.java
@@ -1,4 +1,4 @@
-package com.juan.CadastroDeNinjas;
+package com.juan.cadastrodeninjas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
