0aa86ace681e68c7a890045e21f84c5475096996
JuanCarlos <juan.carlos.marques.pereira@gmail.com>
2026-07-02 21:06:49 -0300

    feat: add Lombok annotations to entity NinjaModel and MissoesModel

--- src/main/java/com/juan/cadastrodeninjas/missoes/model/MissoesModel.java
+++ src/main/java/com/juan/cadastrodeninjas/missoes/model/MissoesModel.java
@@ -3,9 +3,17 @@ package com.juan.cadastrodeninjas.missoes.model;
 
 import com.juan.cadastrodeninjas.ninjas.model.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table (name = "tb_missoes")
 public class MissoesModel {
@@ -21,47 +29,5 @@ public class MissoesModel {
     // @OneToMany - Uma missão pode ter vários ninjas
     @OneToMany (mappedBy = "missoes")
     private List<NinjaModel> ninjas;
-
-    public MissoesModel() {
-    }
-
-    public MissoesModel(Long id, String titulo, String descricao, List<NinjaModel> ninjas) {
-        this.id = id;
-        this.titulo = titulo;
-        this.descricao = descricao;
-        this.ninjas = ninjas;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public List<NinjaModel> getNinjas() {
-        return ninjas;
-    }
-
-    public void setNinjas(List<NinjaModel> ninjas) {
-        this.ninjas = ninjas;
-    }
 }
 
--- src/main/java/com/juan/cadastrodeninjas/ninjas/model/NinjaModel.java
+++ src/main/java/com/juan/cadastrodeninjas/ninjas/model/NinjaModel.java
@@ -3,8 +3,15 @@ package com.juan.cadastrodeninjas.ninjas.model;
 
 import com.juan.cadastrodeninjas.missoes.model.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "tb_cadastro")
 public class NinjaModel {
@@ -23,37 +30,4 @@ public class NinjaModel {
     @ManyToOne
     @JoinColumn(name = "missoes_id") //Foreing key ou chave estrangeira
     private MissoesModel missoes;
-
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
