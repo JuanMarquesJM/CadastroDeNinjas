c15a0c10fc5c40e0ffc917f8c7319096002667d0
JuanCarlos <juan.carlos.marques.pereira@gmail.com>
2026-07-13 22:40:41 -0300

    fix: add @Column annotation to id, imgUrl,nome and idade fields

--- src/main/java/com/juan/cadastrodeninjas/ninjas/model/NinjaModel.java
+++ src/main/java/com/juan/cadastrodeninjas/ninjas/model/NinjaModel.java
@@ -18,17 +18,23 @@ public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
+    @Column(name = "nome")
     private String nome;
 
     @Column(unique = true)
     private String email;
 
+    @Column(name = "img_url")
+    private String imgUrl;
+
+    @Column(name = "idade")
     private Integer idade;
 
     // @ManyToOne - Um ninja tem uma única missão
     @ManyToOne
-    @JoinColumn(name = "missoes_id") //Foreing key ou chave estrangeira
+    @JoinColumn(name = "missoe s_id") //Foreing key ou chave estrangeira
     private MissoesModel missoes;
 }
